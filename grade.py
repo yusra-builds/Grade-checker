a30acc4232a0af0fffb620b128414151e03e2779
Yusra <aklekaryusra@email.com>
2026-04-22 14:40:45 +0530

    First commit

---
+++ grade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks:"))
+
+if (marks>=90):
+    print("Grade A")
+elif(marks>=80):
+    print("Grade B")
+elif (marks>=70):
+    print("Grade C")
+elif (marks>=60):
+    print("Grade D")
+elif (marks>=50):
+    print("Grade E")
+else:
+    print("No Grade")
